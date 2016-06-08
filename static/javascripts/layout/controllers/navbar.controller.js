5725201515b17df3dd88fa175986d82815b63cc5
Julian Kates-Harbeck <julian@Julians-MacBook-Air.local>
2016-06-08 17:07:12 -0400

    first commit

---
+++ static/javascripts/layout/controllers/navbar.controller.js
@@ -0,0 +1,37 @@
+/**
+* NavbarController
+* @namespace thinkster.layout.controllers
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.layout.controllers')
+    .controller('NavbarController', NavbarController);
+
+  NavbarController.$inject = ['$scope', 'Authentication'];
+
+  /**
+  * @namespace NavbarController
+  */
+  function NavbarController($scope, Authentication) {
+    var vm = this;
+
+    vm.logout = logout;
+
+    /**
+    * @name logout
+    * @desc Log the user out
+    * @memberOf thinkster.layout.controllers.NavbarController
+    */
+    function logout() {
+      console.log('hi');
+      Authentication.logout();
+    }
+
+    function print_msg() {
+      console.log('hi');
+    }
+
+  }
+})();
